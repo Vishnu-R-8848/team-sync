a1958ee4f824f613db37c1845b259f6b3dad1fd9
Vishnu R <vishnu6364748848@gmail.com>
2026-06-17 11:35:54 +0530

    style: complete theme system

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/states/auth/authSlice";
+import themeReducer from "../shared/states/themeSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    // theme: themeReducer,
+    theme: themeReducer,
   },
 });
---
+++ src/shared/states/themeSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    mode: localStorage.getItem("theme")
+      ? localStorage.getItem("theme")
+      : "light",
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+      localStorage.setItem("theme", state.mode);
+    },
+  },
+});
+
+export let { toggleTheme } = themeSlice.actions;
+export default themeSlice.reducer;

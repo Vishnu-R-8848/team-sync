a711530ca40dca0037b78cfed1fea19c552d98df
Vishnu R <vishnu6364748848@gmail.com>
2026-06-17 14:43:26 +0530

    feat: complete axios auth interceptors

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -4,3 +4,22 @@ export const axiosInstance = axios.create({
   baseURL: "https://api.team-sync.space/api",
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (response) => response,
+  async (error) => {
+    let originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+    }
+
+    try {
+      await axiosInstance.get("/auth/get-accessToken");
+      return axiosInstance(originalRequest);
+    } catch (error) {
+      window.location.href = "/";
+      return Promise.reject(error);
+    }
+  },
+);
